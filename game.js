1ae3057a4dd7aca8c86db5bb9f86bb07a7bf72b0
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-16 13:50:15 -0600

    added playRound()

--- game.js
+++ game.js
@@ -3,3 +3,24 @@ function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[intBetweenZeroAndTwo];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if(String.toUpper(playerSelection)===String.toUpper(computerSelection)) 
+        return "It's a tie! You both chose: " + playerSelection;
+    else switch(String.toUpper(playerSelection)) {
+        case "ROCK":
+            if(String.toUpper(computerSelection) === "PAPER")
+                return "You lose! Paper beats Rock."
+            else
+                return "You win! Rock beats Scissors!"
+        case "PAPER":
+            if(String.toUpper(computerSelection) === "SCISSORS")
+                return "You lose! Scissors beats paper."
+            else
+                return "You win! Paper beats Rock!"
+        case "SCISSORS":
+            if(String.toUpper(computerSelection) === "PAPER")
+                return "You lose! Paper beats Rock."
+            else
+                return "You win! Rock beats Scissors!"
+}
