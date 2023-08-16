bbed8a9a8fc0bb8a4ca08cc2d59cbd7bd7dc0d6b
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-16 14:45:57 -0600

    added main gameplay function game()

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+const NUM_OF_ROUNDS = 5;
+let playerWinCounter = 0;
+let computerWinCounter = 0;
+
 function getComputerChoice() {
     const intBetweenZeroAndTwo = Math.floor(Math.random()*3);
     const choices = ["Rock", "Paper", "Scissors"];
@@ -9,19 +13,42 @@ function playRound(playerSelection, computerSelection) {
         return "It's a tie! You both chose: " + playerSelection;
     else switch(playerSelection.toUpperCase()) {
         case "ROCK":
-            if(computerSelection.toUpperCase() === "PAPER")
+            if(computerSelection.toUpperCase() === "PAPER") {
+                computerWinCounter++;
                 return "You lose! Paper beats Rock."
-            else
+            }
+            else {
+                playerWinCounter++;
                 return "You win! Rock beats Scissors!"
+            }
         case "PAPER":
-            if(computerSelection.toUpperCase() === "SCISSORS")
+            if(computerSelection.toUpperCase() === "SCISSORS") {
+                computerWinCounter++;
                 return "You lose! Scissors beats paper."
-            else
+            }
+            else {
+                playerWinCounter++;
                 return "You win! Paper beats Rock!"
+            }
         case "SCISSORS":
-            if(computerSelection.toUpperCase() === "PAPER")
+            if(computerSelection.toUpperCase() === "PAPER") {
+                computerWinCounter++;
                 return "You lose! Paper beats Rock."
-            else
+            }
+            else {
+                playerWinCounter++;
                 return "You win! Rock beats Scissors!"
+            }
+    }
+}
+
+function game() {
+    String playerChoice;
+    console.log("Starting new game");
+    for(let i=0; i < NUM_OF_ROUNDS; i++) {
+        playerChoice = prompt("Rock, Paper, or Scissors?");
+        let result = playRound(playerChoice, getComputerChoice());
+        console.log(result);
+        console.log("Player score: " + playerWinCounter + "  Computer score: " + computerWinCounter);
     }
 }
