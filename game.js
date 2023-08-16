c0f7c70b45b2e9ad3bb4df0c250e042716941df9
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-16 13:06:18 -0600

    created first JS function

---
+++ game.js
@@ -0,0 +1,5 @@
+function getComputerChoice() {
+    const intBetweenZeroAndTwo = Math.floor(Math.random()*3);
+    const choices = {"Rock", "Paper" "Scissors"};
+    return choices[intBetweenZeroAndTwo];
+}
